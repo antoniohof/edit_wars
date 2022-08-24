fa3e3e0129378dacf7c34bf2bf11335e598f3a9d
Alberto Harres <alberto.harres@gmail.com>
2022-08-24 17:04:26 +0200

    implement aframe background

---
+++ data/index.js
@@ -0,0 +1,30 @@
+import { nato_zapad_evrop } from './nato_zapad_evrop.js'
+
+const steps = [
+  {
+    index: 0,
+    data: nato_zapad_evrop,
+    startDate: "01/01/2022",
+    endData: "04/01/2022",
+  },
+  {
+    index: 1,
+    data: nato_zapad_evrop,
+    startDate: "02/01/2022",
+    endData: "05/01/2022",
+  },
+  {
+    index: 2,
+    data: nato_zapad_evrop,
+    startDate: "03/01/2022",
+    endData: "07/01/2022",
+  },
+  {
+    index: 3,
+    data: nato_zapad_evrop,
+    startDate: "04/01/2022",
+    endData: "08/01/2022",
+  }
+] 
+
+export { steps }
