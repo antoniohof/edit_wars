1ae1f88f062fa5ea5649dce0a0a75ad0c87cb085
Antonio Hofmeister Ribeiro <hof.antonio@gmail.com>
2022-09-05 18:52:17 +0200

    Update WordCloud.vue

--- components/WordCloud.vue
+++ components/WordCloud.vue
@@ -3,7 +3,7 @@
     <a-scene embedded>
       <a-entity camera look-controls wasd-controls="acceleration:100; fly: true;" :position="`5 7 ${5 + 4*totalProgress}`"></a-entity>
       <a-sky color="#fffffff"></a-sky>
-      <a-text font="msdf/cyrilic-msdf.json" opacity="0.2" color="black" font-image="msdf/cyrilic.png" negate="false" v-for="n in news" :key="n.ID" :position="`${n.x} ${n.y} ${n.z}`" :value="n.title_new"></a-text>
+      <a-text font="msdf/cyrilic-msdf.json" opacity="0.8" color="black" font-image="msdf/cyrilic.png" negate="false" v-for="n in news" :key="n.ID" :position="`${n.x} ${n.y} ${n.z}`" :value="n.title_new"></a-text>
     </a-scene>
   </client-only>
 </template>
