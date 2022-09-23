7320c25924eebb29020777b8ab585e8508da6afe
Antonio Hofmeister Ribeiro <hof.antonio@gmail.com>
2022-09-23 15:22:17 +0200

    title
    title

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <v-container fluid class="home ma-0 pa-0">
-    Intro
+    <v-container fluid class="intro">
+      The monopoly on information is a key propaganda tool. Using it, a state is able to shape a non-alternative picture of the world.
+
+  Nowadays, not only does the Russian government wage a war in Ukraine, but it also works hard on shaping the information reality using propaganda narratives.
+
+  This project is the data and art research how propaganda narratives are reproduced in the Russian-language digital media in the closed space of destroyed media freedom.
+    </v-container>
   </v-container>
 </template>
 
@@ -54,4 +60,11 @@ export default {
   position: relative
   font-family: Space Mono !important
   font-size: 32px !important
+  align-content: center
+  justify-content: flex-start
+  align-items: center
+.intro
+  width: 80%
+  margin-left: 85px
+
 </style>
