9478441784e162bcecc19023165a4a54ff233b2b
Antonio Hofmeister Ribeiro <hof.antonio@gmail.com>
2022-09-23 17:57:39 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default {
       }
     ],
     link: [
+      { href:"/admin/config.yml", type:"text/yaml", rel:"cms-config-url" },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       /*
       {
