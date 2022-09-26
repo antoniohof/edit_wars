1992d44927701d2eb1bcc85ec9c6a0148796280b
Antonio Hofmeister Ribeiro <hof.antonio@gmail.com>
2022-09-26 19:19:58 +0200

    Update WordCloud.vue

--- components/WordCloud.vue
+++ components/WordCloud.vue
@@ -106,7 +106,7 @@ export default {
   -o-transform: translateZ(0)
   transform: translateZ(0)
 .hide
-  opacity: 0.2 !important
+  opacity: 0.05 !important
   pointer-events: none !important
 </style>
   
