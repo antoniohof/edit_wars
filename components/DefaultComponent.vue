a7178427cee74f1dff10b3e22a9d104912c1dc12
Antonio Hofmeister Ribeiro <hof.antonio@gmail.com>
2022-10-07 09:06:37 +0200

    add new content
    add new content

--- components/DefaultComponent.vue
+++ components/DefaultComponent.vue
@@ -1,36 +1,75 @@
 <template>
-  <v-container class="step-component" fluid>
-    DEFAULT COMPONENT
-    <nuxt-content class='step-component_content' :document="step[currentBody]" />
-    {{progress}}
+  <v-container class="text-component" fluid ma-0 pa-0>
+    <div class="text-component_container">
+      <div class="text-component_container_top"></div>
+      <h1 class="text-component_container_title" v-if="hasTitle">
+        {{ currentTitle }}
+      </h1>
+      <nuxt-content
+        class="text-component_container_content"
+        :document="step[currentBody]"
+      />
+    </div>
   </v-container>
 </template>
 
 <script>
-// in case still haven't programmed a component for this step, this one will be loaded
-import StepMixin from "@/mixins/StepMixin.js";
+import StepMixin from '@/mixins/StepMixin.js'
 
 export default {
-  name: 'DefaultComponent',
+  name: 'DefaultText',
   mixins: [StepMixin],
-  computed: {
+  data() {
+    return {}
   },
-  methods: {
+  mounted() {
+    console.log('montou')
   },
-  watch: {
-  }
+  async asyncData({ $content }) {},
+  computed: {},
+  methods: {},
+
+  watch: {}
 }
 </script>
 
 <style lang="sass" scoped>
-.step-component
+
+.text-component
   display: flex
   flex-direction: column
-  align-content: flex-start
-  width: 100%
-  margin-bottom: 200px
-  color: red
-  &_content
-    color: red
-    background-color: yellow
+  align-content: center
+  justify-content: center
+  width: fit-content
+  height: 100vh
+  will-change: transform
+  @media only screen and (max-width: 480px)
+    align-items: flex-start
+    justify-content: flex-start
+    align-items: flex-start
+    align-content: flex-start
+  &_container
+    border-radius: 4px
+    background-color: $window-text-bg
+    backdrop-filter: blur(3px)
+    box-shadow: 0px 4px 25px 0px #00000040
+    &_top
+      border-radius: 4px 4px 0px 0px
+      height: 24px
+      width: 100%
+      background-color: $window-top-bg
+    &_title
+      color: white
+      font-weight: 900
+      font-size: 22px
+      margin-bottom: 10px
+    &_content
+      padding: 12px
+      height: fit-content
+      width: 100%
+      :deep(p)
+        font-family: Golos-Text-Regular
+        color: black
+        font-size: 16px
+        margin-bottom: 0px
 </style>
--- utils/constants.js
+++ utils/constants.js
@@ -4,15 +4,22 @@ narratives[1] = {
   name: '“Freezing Europe”',
   slug: 'freezing-europe'
 }
+/*
 narratives[2] = {
   id: 2,
   name: '“Protecting” the russian world',
   slug: 'protecting-russian-world'
 }
-narratives[4] = {
-  id: 4,
+*/
+narratives[3] = {
+  id: 3,
   name: 'Mythical “Nazis”',
   slug: 'mythical-nazis'
+}
+narratives[4] = {
+  id: 4,
+  name: '“cold-war-ii',
+  slug: 'cold-war-ii'
 }
 
 const colorPalette = ['#FFBE41', '#A67BEA', '#66CE79', '#FF70B6']
