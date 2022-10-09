5fe407fbef4a1d6a11f4016ce89492a9c1f72871
Antonio Hofmeister Ribeiro <hof.antonio@gmail.com>
2022-10-09 19:11:07 +0200

    Update BarChart.vue

--- components/BarChart.vue
+++ components/BarChart.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="graph-container">
     <p class="chart-title" v-show="background">{{ background.chart_title }}</p>
+    <client-only>
     <Scatter
       v-if="currentChartData"
       :chart-options="chartOptions"
@@ -12,12 +13,13 @@
       :width="width"
       :height="height"
     />
+    </client-only>
     <p class="chart-description" v-show="background">{{ background.description }}</p>
   </div>
 </template>
 
 <script>
-import { Bar, Scatter, Line } from "vue-chartjs";
+import { Bar, Scatter } from "vue-chartjs";
 import {
   Chart as ChartJS,
   Title,
@@ -79,8 +81,7 @@ export default {
   mixins: [StepMixin],
   components: {     
     Bar, 
-    Scatter,
-    Line
+    Scatter
 },
   props: {
     background: {
